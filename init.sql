4c3f8104bcc83835b3af39beb41afc0ebe1c4f93
fpiacentini-flx <federico.piacentini@flexibility.com.ar>
2024-02-14 14:53:55 -0300

    Initial commit. Get all products

---
+++ init.sql
@@ -0,0 +1,3 @@
+INSERT INTO products.product (name, price, quantity) VALUES('A', 0.5, 3);
+INSERT INTO products.product (name, price, quantity) VALUES('B', 3.0, 10);
+INSERT INTO products.product (name, price, quantity) VALUES('C', 0.15, 30);
