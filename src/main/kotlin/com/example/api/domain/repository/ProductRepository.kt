c810467fcdeb03e6eb523fd0b475d6e24b662a2c
fpiacentini-flx <federico.piacentini@flexibility.com.ar>
2024-02-24 07:50:32 -0300

    Do not allow negative stock quantity

--- src/main/kotlin/com/example/api/domain/repository/ProductRepository.kt
+++ src/main/kotlin/com/example/api/domain/repository/ProductRepository.kt
@@ -1,7 +1,6 @@
 package com.example.api.domain.repository
 
 import com.example.api.domain.model.Product
-import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
