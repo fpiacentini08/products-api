3d63e6d144438d2f95c139f95e0526b596e07439
fpiacentini-flx <federico.piacentini@flexibility.com.ar>
2024-02-24 07:44:02 -0300

    Test stock service

--- src/main/kotlin/com/example/api/ApiApplication.kt
+++ src/main/kotlin/com/example/api/ApiApplication.kt
@@ -1,12 +1,7 @@
 package com.example.api
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 open class ApiApplication
